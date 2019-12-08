3114dc0849f957e3fcc0147e58a2124180322cda
stooory_888 <khaledstooory@gmail.com>
2019-12-08 03:29:37 -0800

    first commit

---
+++ khaled.php
@@ -0,0 +1,42 @@
+﻿<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en"><head><title>webpage title</title>
+
+<meta http-equiv="content-type" content="text/html; charset=utf-8" />
+<meta name="description" content="" />
+<meta name="keywords" content="" />
+<meta name="robots" content="all" />
+<meta name="author" content="" />
+<link rel="stylesheet" href="css/mainStyle.css" type="text/css" media="screen" />
+</head>
+
+<body>
+<div id="header">
+<h1 id="logo"> <a href="#" title="back to index page">Curriculum<span class="logoDot"></span>Vitae</a>
+</h1>
+
+</div>
+<lu>
+<div id="container">
+<div id="wrapper">
+<div id="addColumn">
+<h1><left>C.V</left></h1>
+<h1><left>Personal Information</left></h1>
+<img src="khaled.JPG" width="100" height="100"</img>
+<li>Name:khaled algliy alsdeeg alemam</li>
+<li>Date of Birth: 15/5/1993 </li>
+<li>Place of Birth: aljazera
+<li>Id no:004424482 </li>
+<li>Mobile :+249923203324 </li>
+<li>E-mail :khaledstooory@gmail.com</li>
+<h1><left>Education</left></h1>
+<li>Name of University: Emirates Collage Of Siecnce & Technology</li>
+<li>Major :Information Technology</li>
+<li>Minor:Information Technology </li>
+<li>GPA :very Good </li> 
+<li>Year :2017</li>
+</lu>
+</div>
+</div>
+</div>
+
+</body></html>
